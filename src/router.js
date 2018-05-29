5a6eda53bfad6333c23b36f27ef09e5a83cb529d
guodong <gd@tongjo.com>
2018-05-29 21:04:58 +0800

    update

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import IndexPage from './routes/IndexPage';
 import Library from "./routes/Library";
 import Signin from "./routes/Signin";
 import Callback from "./routes/Callback";
-import KongfuEditor from "./routes/KongfuEditor";
+// import KongfuEditor from "./routes/KongfuEditor";
 import KongfuReader from "./routes/KongfuReader";
 
 function RouterConfig({ history }) {
@@ -15,7 +15,7 @@ function RouterConfig({ history }) {
         <Route path="/library" exact component={Library}/>
         <Route path='/signin' exact component={Signin}/>
         <Route path='/callback' component={Callback}/>
-        <Route path='/editor/:kongfu_id' component={KongfuEditor}/>
+        {/*<Route path='/editor/:kongfu_id' component={KongfuEditor}/>*/}
         <Route path='/reader/:kongfu_id' component={KongfuReader}/>
       </Switch>
     </Router>
